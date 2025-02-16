35b7d5957ce9c8a2daa99dc4ff07c6badf76ba6a
ammayi <ammayi101@gmail.com>
2025-02-16 14:51:29 +0100

    linking with Mongodb database

--- books/pipelines.py
+++ books/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
 
 
-class BooksPipeline:
+class MongodbPipeline:
+    collection_name = "All_books"
+    
+    
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient("mongodb+srv://chimeraor:JwT3L8HWZEw39SUs@cluster0.j67ex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        self.db = self.client["Books"]
+    
+    def close_spider(self, spider):
+        self.client.close()
+
+    
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
         return item
