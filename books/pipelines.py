cf21193449ab414ead935879c758fc52b08f8675
ammayi <ammayi101@gmail.com>
2025-02-16 15:23:04 +0100

    storing scraped data to a sqllite data base

--- books/pipelines.py
+++ books/pipelines.py
@@ -6,22 +6,51 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-import pymongo
+#import pymongo
+import sqlite3
 
-
-class MongodbPipeline:
-    collection_name = "All_books"
+# class MongodbPipeline:
+#     collection_name = "All_books"
     
     
 
+#     def open_spider(self, spider):
+#         self.client = pymongo.MongoClient("mongodb+srv://chimeraor:JwT3L8HWZEw39SUs@cluster0.j67ex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+#         self.db = self.client["Books"]
+    
+#     def close_spider(self, spider):
+#         self.client.close()
+
+    
+#     def process_item(self, item, spider):
+#         self.db[self.collection_name].insert_one(dict(item))
+#         return item
+    
+class SQLlitePipeline:  
+    
     def open_spider(self, spider):
-        self.client = pymongo.MongoClient("mongodb+srv://chimeraor:JwT3L8HWZEw39SUs@cluster0.j67ex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        self.db = self.client["Books"]
-    
+        self.connection = sqlite3.connect("Books.db")
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute('''
+                CREATE TABLE All_Books(
+                        title TEXT,
+                        price TEXT
+                        )              
+            ''')
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
     def close_spider(self, spider):
-        self.client.close()
-
+        self.connection.close()
+        
     
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert_one(dict(item))
+        self.c.execute('''
+            INSERT INTO ALL_Books (title, price) VALUES(?,?)
+
+        ''', (item.get('title'),
+              item.get('price')))
+        self.connection.commit()
         return item
+
--- books/settings.py
+++ books/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "books.pipelines.MongodbPipeline": 300,
+    "books.pipelines.SQLlitePipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
